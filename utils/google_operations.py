88f26987f5cb2ce8549095dc10b820765ab84d2c
vijaykumar <vijaykumarrdhanapal@gmail.com>
2023-05-23 13:53:16 +0530

    remove space function

--- utils/google_operations.py
+++ utils/google_operations.py
@@ -2,6 +2,7 @@
 import json
 from settings import config_json
 from settings import logger
+import re
 
 
 from google.cloud import bigquery
@@ -12,10 +13,16 @@ project_id = config_json["gbq_config"]["project"]
 client = bigquery.Client(credentials=credentials,project=project_id)
 
 
+def remove(string):
+    pattern = re.compile(r'\s+')
+    return re.sub(pattern,' ',string)
+
+
 def get_crime_data(report_type,time_period):
     logger.info('entering get_crime_data function')
     logger.debug('printing sql query')
-    report_type=report_type.capitalize()
+    report_type = report_type.capitalize()
+    remove(report_type)
     report_sql=f"""SELECT *            
     FROM bigquery-public-data.austin_crime.crime 
     WHERE primary_type = '{report_type}' AND  
