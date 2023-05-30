bd0a50625c0424e809816f9c81098c9ae7163cf8
Vijay <vijaykumarrdhanapal@gmail.com>
2023-05-30 19:30:58 +0530

    file name location changed

--- send_crime_reports.py
+++ send_crime_reports.py
@@ -1,8 +1,10 @@
-from utils import email_operations,google_operations,report_operations
+from utils import report_operations
 from settings import logger
+from settings import config_json
+
 
 logger.info('calling read_report_csv function from main module')
-read_dataframe = report_operations.read_report_csv('D:\\projects\\big_query\\email.csv')
+read_dataframe = report_operations.read_report_csv(config_json['app_config']['input_filename'])
 
 logger.info('calling processing_report_file function from main module')
 report_operations.process_report_request(read_dataframe)
